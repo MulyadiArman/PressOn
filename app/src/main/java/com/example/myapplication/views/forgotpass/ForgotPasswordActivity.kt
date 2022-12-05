005a259be50609f9d3dd8fc549258d9aca060dfd
MulyadiArman <mulyadiarman18@gmail.com>
2022-12-05 11:30:42 +0800

    memperbaiki maps

--- app/src/main/java/com/example/myapplication/views/forgotpass/ForgotPasswordActivity.kt
+++ app/src/main/java/com/example/myapplication/views/forgotpass/ForgotPasswordActivity.kt
@@ -1,9 +1,30 @@
 package com.example.myapplication.views.forgotpass
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Handler.Callback
+import android.os.Looper
+import android.util.Log
+import android.util.Patterns
 import com.example.myapplication.R
 import com.example.myapplication.databinding.ActivityForgotPasswordBinding
+import com.example.myapplication.dialog.MyDialog
+import com.example.myapplication.dialog.dynamicDialog
+import com.example.myapplication.dialog.hideDialog
+import com.example.myapplication.dialog.showProgressDialog
+import com.example.myapplication.model.ForgotPasswordResponse
+import com.example.myapplication.networking.ApiServices
+import com.example.myapplication.networking.RetrofitClient
+import com.google.gson.Gson
+import okhttp3.ResponseBody
+
+import retrofit2.Call
+import retrofit2.Converter
+import retrofit2.Response
+import java.io.IOException
+
 
 class ForgotPasswordActivity : AppCompatActivity() {
 
@@ -15,19 +36,103 @@ class ForgotPasswordActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         init()
-        //Tambahkan Function onClick
         onClick()
     }
 
-    //Function Onclick
     private fun onClick() {
         binding.tbForgotPassword.setNavigationOnClickListener {
             finish()
         }
+
+        binding.btnForgotPassword.setOnClickListener {
+            val email = binding.etEmailForgotPassword.text.toString()
+            if (isFormValid(email)){
+                forgotPassToServer(email)
+            }
+        }
+    }
+
+    private fun forgotPassToServer(email: String) {
+        val forgotPasswordRequest = ForgotPasswordRequest(email = email)
+        val forgotPasswordRequestString = Gson().toJson(forgotPasswordRequest)
+
+        MyDialog(showProgressDialog(this))
+
+        ApiServices.getLiveAttendanceServices()
+            .forgotPasswordRequest(forgotPasswordRequestString)
+            .enqueue(object : retrofit2.Callback<ForgotPasswordResponse> {
+                override fun onResponse(
+                    call: Call<ForgotPasswordResponse>,
+                    response: Response<ForgotPasswordResponse>
+                ) {
+                    MyDialog(hideDialog())
+                    if (response.isSuccessful){
+                        val message = response.body()?.message
+                        MyDialog(dynamicDialog(this@ForgotPasswordActivity, getString(R.string.success),message.toString()))
+//                        MyDialog.dynamicDialog(
+//                            this@ForgotPasswordActivity,
+//                            getString(R.string.success),
+//                            message.toString()
+//                        )
+                        Handler(Looper.getMainLooper()).postDelayed({
+                            MyDialog(hideDialog())
+                            finish()
+                        },2000)
+                    }else{
+                        val errorConverter: Converter<ResponseBody, ForgotPasswordResponse> =
+                            RetrofitClient
+                                .getClient()
+                                .responseBodyConverter(
+                                    ForgotPasswordResponse::class.java,
+                                    arrayOfNulls<Annotation>(0)
+                                )
+                        var errorResponse: ForgotPasswordResponse?
+                        try {
+                            response.errorBody()?.let {
+                                errorResponse = errorConverter.convert(it)
+                                MyDialog(dynamicDialog(
+                                    this@ForgotPasswordActivity,
+                                    getString(R.string.failed),
+                                    errorResponse?.message.toString()
+                                ))
+                            }
+                        }catch (e: IOException){
+                            e.printStackTrace()
+                            Log.e(TAG, "Error: ${e.message}")
+                        }
+                    }
+                }
+
+                override fun onFailure(call: Call<ForgotPasswordResponse>, t: Throwable) {
+                    MyDialog(hideDialog())
+                    Log.e(TAG, "Error: ${t.message}")
+                }
+
+            })
+    }
+
+    private fun isFormValid(email: String): Boolean {
+        if (email.isEmpty()){
+            binding.etEmailForgotPassword.error = getString(R.string.please_field_your_email)
+            binding.etEmailForgotPassword.requestFocus()
+        }else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            binding.etEmailForgotPassword.error = getString(R.string.please_use_valid_email)
+            binding.etEmailForgotPassword.requestFocus()
+        }else{
+            binding.etEmailForgotPassword.error = null
+            return true
+        }
+        return false
     }
 
     private fun init() {
         setSupportActionBar(binding.tbForgotPassword)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
+
+    companion object{
+        private val TAG = ForgotPasswordActivity::class.java.simpleName
+    }
 }
+
+
